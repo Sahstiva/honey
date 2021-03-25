46b3b1df35c430fc76d58a054a61ef154185e2f5
Sahstiva <avitshas@gmail.com>
2021-03-25 08:40:51 +0300

    Добавлена серверная часть на php

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
-const API = 'http://127.0.0.1:5500/api';
+// const API = 'http://127.0.0.1:5500/api';
+const API = 'https://www.vitshas.me/portfolio/honey/api/card';
 
 const app = new Vue({
     el: '#app',
     data: {
-        dataUrl: '/data.json',
+        readUrl: '/read.php',
+        createUrl: '/create.php',
         cards: [],
         userName: '',
         userEmail: '',
-        userMessage: ''
+        userMessage: '',
+        popupShow: false
     },
     methods: {
     getJson(url) {
@@ -17,23 +20,53 @@ const app = new Vue({
                 console.log(error);
             })
         },
+    postJson(data, url) {
+        return fetch(url, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(result => {
+            let res = result.json();
+            alert(res);
+            return res; })
+        .catch(error => {
+            alert(error);
+            console.log(error) });
+
+        },
     addCard() {
         if(this.userName && this.userEmail && this.userMessage) {
-            let newID = this.cards.reduce( (sum,item) => sum + item.id, 0)
-            let item = {
-                "id": newID,
+            let objCard = {
                 "name": this.userName,
                 "email": this.userEmail,
-                "message": this.userMessage,
-                "itemtheme": `card-body content__item-${ newID % 2 ? "green" : "gray"}`,
-                "headingtheme": `card-title content__heading content__heading-${ newID % 2 ? "green" : "gray"}`
+                "message": this.userMessage
             };
-            this.cards.push(item);
+            this.postJson(objCard,`${API + this.createUrl}`)
+                .then(result => {
+                    let newID = this.cards.reduce( (sum,item) => sum + item.id, 0)
+                    let itemtheme = `card-body content__item-${ newID % 2 ? "green" : "gray"}`;
+                    let headingtheme = `card-title content__heading content__heading-${ newID % 2 ? "green" : "gray"}`;
+                    let item = Object.assign({ "itemtheme": itemtheme, "headingtheme": headingtheme }, objCard);
+                    this.cards.push(item);
+                    return this.popupShow = true;
+                            // if(result)
+                    //     console.log(result);
+                });
             }
-        }
+        },
+    clearForm() {
+        this.userName = '';
+        this.userEmail = '';
+        this.userMessage = '';
+        this.popupShow = !this.popupShow;
+    }
     },
     mounted() {
-        this.getJson(`${API + this.dataUrl}`)
+        this.getJson(`${API + this.readUrl}`)
             .then(data => {
                 for (let el of data) {
                     let itemtheme = `card-body content__item-${ el.id % 2 ? "green" : "gray"}`;
